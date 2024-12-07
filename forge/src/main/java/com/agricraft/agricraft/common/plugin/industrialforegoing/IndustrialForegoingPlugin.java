63fc6292f40332e25ace5f4dc1362659cefa6d9b
PiotrO15 <72670117+PiotrO15@users.noreply.github.com>
2024-12-07 14:56:28 +0100

    Allow the plugin to be run on dedicated servers

--- forge/src/main/java/com/agricraft/agricraft/common/plugin/industrialforegoing/IndustrialForegoingPlugin.java
+++ forge/src/main/java/com/agricraft/agricraft/common/plugin/industrialforegoing/IndustrialForegoingPlugin.java
@@ -3,13 +3,12 @@ package com.agricraft.agricraft.common.plugin.industrialforegoing;
 import com.agricraft.agricraft.api.AgriApi;
 import com.agricraft.agricraft.api.config.CompatConfig;
 import com.buuz135.industrial.registry.IFRegistries;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegisterEvent;
 
-@Mod.EventBusSubscriber(modid = AgriApi.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = AgriApi.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class IndustrialForegoingPlugin {
     @SubscribeEvent
     public static void registerAgriCraftPlantRecollectable(RegisterEvent event) {
